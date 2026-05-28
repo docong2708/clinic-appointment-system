fba6eab328137a8656ccd2bff619961f4f452c35
Cong Do <dxcong2708@gmail.com>
2026-05-28 14:35:09 +0700

    initialize project microservices

---
+++ services/doctor-service/src/test/java/com/group01/doctor/DoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group01.doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shared/common-web/src/test/java/com/group01/commonweb/CommonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group01.commonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
